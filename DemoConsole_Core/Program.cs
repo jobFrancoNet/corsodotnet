48072d8f72273ecc88c5671313841709ed5f9475
FRANCESCO SPALLUZZI <aiutocorsi@hotmail.it>
2020-12-08 17:56:44 +0100

    Aggiungere i file di progetto.

---
+++ DemoConsole_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoConsole_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
