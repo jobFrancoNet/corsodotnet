6de82eb370ec7b68e241328c0825311cb757c886
FRANCESCO SPALLUZZI <aiutocorsi@hotmail.it>
2020-12-23 18:16:04 +0100

    Update progetto asp.net core

---
+++ demo_aspcore/Controller/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo_aspcore.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Welcome01()
+        {
+            return "This is the Welcome01 action method...";
+        }
+    }
+}
